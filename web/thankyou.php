e6aac98d8bd56a03bda1a919bcaa12caf9fa20d0
Fumikazu Fujiwara <github@ze.gs>
2015-09-13 07:09:38 +0900

    add thankyou.php

---
+++ web/thankyou.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+  <title>Thank you</title>
+</head>
+
+<body>
+<?php
+require '../config/db.php';
+require '../src/model/Invitation.php';
+$obj = new Invitation(); 
+try{
+    $obj -> create(trim($_GET['name']));
+    echo 'Thank you!!';
+
+}catch(Exception $e){
+    echo $e -> getMessage();
+}
+?>
+</body>
+</html>
