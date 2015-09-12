355fc44509f18d5d1a055880e148a08b6c5d8d1b
Fumikazu Fujiwara <github@ze.gs>
2015-09-12 15:05:25 +0900

    validateUserId

--- src/model/Invitation.php
+++ src/model/Invitation.php
@@ -1,7 +1,10 @@
 <?php
 
 class Invitation {
-    public function issueAHash($length = 8){
+    public function issueAHash($length = 16){
         return substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, $length);
+    }
+    public function validateForUserId($userId){
+        return 1 == preg_match('/^[a-zA-Z0-9]+$/',$userId);
     }
 }
