9ff2e81ab8261509b038389cae576acc763898d8
Fumikazu Fujiwara <github@ze.gs>
2015-09-13 08:59:14 +0900

    bug fixed

--- web/create.php
+++ web/create.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-
-  <title>Create Your Account</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-  <form action="thankyou.php" method="post">
-    <input type="hidden" name="hash" value=
-    "<?php echo htmlspecialchars($_GET['hash'], ENT_QUOTES);?>" />
-    <input name="user_id" type="text" /> <input type="submit" value=
-    "Create Account" />
-  </form>
-</body>
-</html>
 <!DOCTYPE html>
 <html lang="en">
   <head>
