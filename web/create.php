f6fa445deb5c54f00f58030d0e7371b3a388c03a
Fumikazu Fujiwara <github@ze.gs>
2015-09-13 06:54:50 +0900

    add create.php

---
+++ web/create.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
+
+<html>
+<head>
+  <title>Create Your Account</title>
+</head>
+
+<body>
+  <form action="thankyou.php" method="post">
+    <input type="text"> <input type="button" value=
+    "Create Account">
+  </form>
+</body>
+</html>
