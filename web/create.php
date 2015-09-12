671b17e60676c296074a77b6f073433a7a4c28b2
Fumikazu Fujiwara <github@ze.gs>
2015-09-13 07:03:48 +0900

    clearn by tidy

--- web/create.php
+++ web/create.php
@@ -1,16 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
   <title>Create Your Account</title>
 </head>
 
 <body>
   <form action="thankyou.php" method="post">
-    <input type="text" name="user_id">
-    <input type="hidden" name="hash" value="<?php echo htmlspecialchars($_GET['hash'], ENT_QUOTES);?>"> 
-    <input type="text"> <input type="button" value=
-    "Create Account">
+    <input type="hidden" name="hash" value=
+    "<?php echo htmlspecialchars($_GET['hash'], ENT_QUOTES);?>" />
+    <input type="text" /> <input type="button" value=
+    "Create Account" />
   </form>
 </body>
 </html>
