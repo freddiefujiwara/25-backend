4533f1e3870d36b2ceefc91d632ff16f84e198eb
Fumikazu Fujiwara <github@ze.gs>
2015-09-13 07:25:11 +0900

    add login_and_issue.php

--- web/index.php
+++ web/index.php
@@ -1,8 +1,16 @@
-<?php
-require '../config/db.php';
-require '../src/model/Invitation.php';
-$ins = new Invitation();
-$ins -> issueAHash();
-echo getenv('DATABASE_URL');
-echo getenv('TABLE_NAME');
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+  <title>Login and Issue</title>
+</head>
+
+<body>
+  <form action="login_and_issue.php" method="post">
+    <input name="user_id" type="text" /> <input type="submit" value=
+    "Login and Issue" />
+  </form>
+</body>
+</html>
---
+++ web/login_and_issue.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+  <title>Issued</title>
+</head>
+
+<body>
+<?php
+require '../config/db.php';
+require '../src/model/Invitation.php';
+$obj = new Invitation(); 
+try{
+    $hash = $obj -> login_and_issue(trim($_POST['user_id']));
+    echo $hash;
+
+}catch(Exception $e){
+    echo $e -> getMessage();
+}
+?>
+</body>
+</html>
